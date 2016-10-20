a9846ef8447b0bb37059b002da96a061f3f568d9
darthranjith <sivasakthisoftwares@AppleDev.local>
2016-10-20 14:15:21 +0530

    Minor changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,26 +11,30 @@ import { Chart } from 'ng2-chartjs2';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-  }
 
-  ionViewDidLoad() {
-    console.log('Hello SalesGraph Page');
   }
 
   labels: string[] = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
   data: Chart.Dataset[] = [
     {
-      label: '# of Votes1',
-      data: [1, 9, 13, 15, 12, 13],
-      backgroundColor: ['rgba(54, 162, 235, 0.2)'],
-      borderColor: ['rgba(54, 162, 235, 1)'],
-      borderWidth: 1
-    },
-    {
-      label: '# of Votes2',
-      data: [11, 19, 3, 5, 2, 3],
-      backgroundColor: ['rgba(255, 206, 86, 0.2)'],
-      borderColor: ['rgba(54, 162, 235, 1)'],
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
       borderWidth: 1
     }
   ];
