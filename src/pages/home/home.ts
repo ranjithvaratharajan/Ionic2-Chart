9bed723e6929883df25eb2674cf17048931f56f1
darthionic <darthionic@icloud.com>
2016-10-30 22:53:51 +0530

    With Options

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,28 +14,55 @@ export class HomePage {
 
   }
 
-  labels: string[] = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
-  data: Chart.Dataset[] = [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
+  // labels: string[] = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+  // data: Chart.Dataset[] = [
+  //   {
+  //     label: '# of Votes',
+  //     data: [12, 19, 3, 5, 2, 3],
+  //     backgroundColor: [
+  //       'rgba(255, 99, 132, 0.2)',
+  //       'rgba(54, 162, 235, 0.2)',
+  //       'rgba(255, 206, 86, 0.2)',
+  //       'rgba(75, 192, 192, 0.2)',
+  //       'rgba(153, 102, 255, 0.2)',
+  //       'rgba(255, 159, 64, 0.2)'
+  //     ],
+  //     borderColor: [
+  //       'rgba(255,99,132,1)',
+  //       'rgba(54, 162, 235, 1)',
+  //       'rgba(255, 206, 86, 1)',
+  //       'rgba(75, 192, 192, 1)',
+  //       'rgba(153, 102, 255, 1)',
+  //       'rgba(255, 159, 64, 1)'
+  //     ],
+  //     borderWidth: 1
+  //   }
+  // ];
+
+  options: any = {
+    type: 'doughnut',
+    data: {
+      labels: ["Restaurante", "Vestuário", "Lazer", "Eletrônico"],
+      datasets: [{
+        label: 'Dinheiro',
+        borderWidth: 0,
+        data: [12, 19, 3, 5],
+        backgroundColor: [
+          '#FDBC11',
+          '#ee4250',
+          '#02A4C0',
+          '#229f37'
+        ],
+      }]
+    },
+    options: {
+      responsive: true,
+      legend: {
+        position: 'left',
+        labels: {
+          boxWidth: 20
+        }
+      }
     }
-  ];
+  };
 }
